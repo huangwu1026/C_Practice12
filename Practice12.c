25b115d3506b42477bfaa8520f1bc63057d1cced
huangwu1026 <2421294458@qq.com>
2019-11-21 23:49:02 +0800

    C_Practice

---
+++ Practice12.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+void hanoi(int n, char one, char two, char three){
+	if (n == 1){
+		printf("%c-->%c\n", one, three);
+	}
+	else
+	{
+		hanoi(n - 1, one, three, two);
+		printf("%c-->%c\n", one, three);
+		hanoi(n - 1, two, one, three);
+	}
+}
+int main(){
+	int n;
+	printf("input the number of diskes:");
+	scanf("%d", &n);
+	hanoi(n, 'A', 'B', 'C');
+	return 0;
+}
